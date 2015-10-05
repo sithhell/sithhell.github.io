684f5bafbc3588e9cb66d7a4d20ef93e863242a7
Thomas Heller <thom.heller@gmail.com>
2015-10-05 13:56:05 +0200

    Adding derivative

--- perf_counters_plot/plot.py
+++ perf_counters_plot/plot.py
@@ -26,6 +26,7 @@ f = open('counters.csv', 'r')
 header = f.readline().replace('\n', '').split(',')
 
 counters = {};
+counters_diff = {};
 
 def get_locality(counter_name):
     return re.findall('locality#\d+', counter_name)[0]
@@ -38,9 +39,12 @@ for h in header:
     counter_name = get_name(h, loc)
     if counter_name in counters:
         counters[counter_name][loc] = []
+        counters_diff[counter_name][loc] = []
     else:
         counters[counter_name] = {};
         counters[counter_name][loc] = []
+        counters_diff[counter_name] = {};
+        counters_diff[counter_name][loc] = []
 
 
 for line in f:
@@ -62,23 +66,38 @@ html_links = '<ul>\n'
 for counter in sorted(counters):
     res = 0.0;
     for data in counters[counter]:
+        counters_diff[counter][data] = np.diff(counters[counter][data])
         res += sum(counters[counter][data])
     if res == 0.0:
         continue
     img_name = counter.replace('/', '_')[1:];
-    img = img_name + '.png'
+    img = img_name + '.svg'
+    img_diff = img_name + '_diff.svg'
     title = counter
     html_links += '<li><a href=#{0}>{1}</a></li>\n'.format(img, title)
     html_images += '<p>\n'
-    html_images += '<a name="{0}"><img src="{0}" alt="{1}"/></a><br/>\n'.format(img, title)
+    html_images += '<a name="{0}"><img src="{0}" alt="{1}" width="49%"/></a>\n'.format(img, title)
+    html_images += '<img src="{0}" alt="{1}" width="49%"/><br/>\n'.format(img_diff, title + ' change')
     html_images += '<a href=#top>Back to top</a>\n'
     html_images += '</p>\n'
     df = pd.DataFrame(counters[counter])
     ax = df.plot()#marker = 'x')
     ax.set_title(title)
     ax.set_xlabel('time')
-    plt.tight_layout()
-    plt.savefig('./' + img)
+
+    lgd = ax.legend(loc='center left', ncol=1, bbox_to_anchor = (1, 0.5))
+    plt.savefig('./' + img, bbox_extra_artists=(lgd,), bbox_inches='tight')
+    plt.close()
+
+    df = pd.DataFrame(counters_diff[counter])
+    ax = df.plot()#marker = 'x')
+    ax.set_title(title + ' change')
+    ax.set_xlabel('time')
+
+    box = ax.get_position()
+
+    lgd = ax.legend(loc='center left', ncol=1, bbox_to_anchor = (1, 0.5))
+    plt.savefig('./' + img_diff, bbox_extra_artists=(lgd,), bbox_inches='tight')
     plt.close()
 
 html += html_links + '</ul>\n';
